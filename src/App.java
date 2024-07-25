e1d58bf9581607c37837a59839b7366c2fbc4a55
KoreaCokuza <duddnd621@gmail.com>
2024-07-26 00:39:09 +0900

    run 함수 세분화

--- src/App.java
+++ src/App.java
@@ -1,32 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class App {
-    void run() {
+    Scanner scanner;
+    int lastQuotationid;
+    List<Quotation> quotations;
 
+    App(){      // run안에서 기능이 너무 많기 때문에 나눠주기 위해 만들었다.
+        lastQuotationid = 0;
+        scanner = new Scanner(System.in);
+        quotations = new ArrayList<>();
+    }
+    void run() {
         System.out.println("== 명언 앱 ==");
-        int lastQuotationid = 0;
+
         while (true) {
             System.out.print("명령) ");
 
-            Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
 
             if (cmd.equals("종료")) {
                 break;
             }
             else if (cmd.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = scanner.nextLine();
-
-                System.out.print("작가 : ");
-                String authorName = scanner.nextLine();
-                lastQuotationid++;
-                int id = lastQuotationid;   // id 값을 구해오는 것.
-                Quotation quotation = new Quotation(id, content, authorName);
-                // Quotation클래스로 id와 명언과 작가를 보낸다.
-
+                actioWrite();
                 System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationid);
             }
+            else if (cmd.equals("목록")) {
+                actionList();
+            }
+        }
+    }
+    void actioWrite(){
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.print("작가 : ");
+        String authorName = scanner.nextLine();
+        lastQuotationid++;
+
+        int id = lastQuotationid;
+        Quotation quotation = new Quotation(id, content,authorName);
+        quotations.add(quotation);
+    }
+
+    void actionList(){
+        System.out.println("번호 / 작가 / 명언");
+
+        System.out.println("----------------------");
+
+        if (quotations.isEmpty()) { // 비어있다면
+            System.out.println("등록된 명언이 없습니다.");    // 비어있다면 출력문 출력.
+        }
+        for (int i = quotations.size() - 1; i >= 0; i--) {
+            Quotation quotation = quotations.get(i);    // 데이터를 가져온다.
+            System.out.printf("%d / %s / %s\n",quotation.id, quotation.authorName, quotation.content);
+            // 처음에 저장된 Quotation클래스에서 정보를 가져온다.
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("안녕하세요. 명언을 등록해 주세요. 목록도 확인이 가능합니다.");
         new App().run();
     }
 }
